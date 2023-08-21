008b0154aa0fa07ba3e2ea079f82bf7588ce70f1
Uros <prijovic.uros13@gmail.com>
2023-08-21 08:36:26 +0200

    Complete login and logout flow using local storage and redux store

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,18 +7,26 @@ import {AuthPage} from "./pages/AuthPage";
 import {LocalizationProvider} from "@mui/x-date-pickers";
 import {AdapterMoment} from "@mui/x-date-pickers/AdapterMoment";
 import {createTheme, ThemeProvider} from "@mui/material";
+import {HomePage} from "./pages/HomePage";
+import {useAppSelector} from "./store/hooks";
+import {selectTheme} from "./store/features/uiSlice";
+import {Notification} from "./components/Notification";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<RootLayout/>}>
+            <Route index={true} element={<HomePage/>}/>
             <Route path="auth/:mode" element={<AuthPage/>}/>
         </Route>
     )
 );
 
 function App() {
+    const theme = useAppSelector(selectTheme);
+
     return (<LocalizationProvider dateAdapter={AdapterMoment}>
-        <ThemeProvider theme={createTheme({palette: {mode: "light"}})}>
+        <ThemeProvider theme={createTheme({palette: {mode: theme}})}>
+            <Notification/>
             <RouterProvider router={router}/>
         </ThemeProvider>
     </LocalizationProvider>);
